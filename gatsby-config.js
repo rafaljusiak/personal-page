da5c5c370f775f460bcc2e6c54e6987afda56795
Rafal Jusiak <rafal.jusiak@jmr.pl>
2021-01-24 02:05:51 +0100

    base config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,20 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: "Rafał Jusiak",
+    siteUrl: "https://rafaljusiak.pl",
+    description: "Web Developer and Software Engineer.",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Oswald`],
+        display: "swap",
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+  ],
+};
